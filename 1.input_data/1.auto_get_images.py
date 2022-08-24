0c317899ba917421457cf15bd3a5923244ac7f78
alalala <alli551617@gmail.com>
2022-08-24 12:54:59 +0800

    ij

--- 1.input_data/1.auto_get_images.py
+++ 1.input_data/1.auto_get_images.py
@@ -2,7 +2,7 @@ import os
 import csv
 import shutil
 from get_images import auto_get_images_1984to2012,auto_get_images_2012to2022
-
+from delete import del_emp_dir
 #輸入圖片路徑
 download_images_path = "C:/git/cbir-project/2.cbir/dataset/"
 
@@ -21,17 +21,22 @@ with open(loaction_path) as location:
     rows = csv.reader(location)
     #跳過第一列
     next(rows)
-
+    next(rows)
     for i in rows:
         print ("-----lon : " + i[0] + "-----lat : "+ i[1] +"-----")
         lon = float(i[0])
         lat = float(i[1])
+        
         #開始抓圖
-        auto_get_images_1984to2012(lon,lat,download_images_path)
-        auto_get_images_2012to2022(lon,lat,download_images_path)         
+        try: 
+            auto_get_images_1984to2012(lon,lat,download_images_path)
+            auto_get_images_2012to2022(lon,lat,download_images_path)  
+        except:
+            del_emp_dir(download_images_path)
+            continue       
 
 #重新命名
 from rename import rename_all
-rename_all(download_images_path)
+#rename_all(download_images_path)
 
 print("-------------finish-------------")
